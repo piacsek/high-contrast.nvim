4eb3deb1e200cdd2e89208101be19322a7562464
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-13 15:38:09 -0700

    another round of porting using Opus 4.6

--- colors/high-contrast.lua
+++ colors/high-contrast.lua
@@ -43,16 +43,33 @@ local colors = {
 	yellow = "#ffcf40",
 	yellow1 = "#ffbf66",
 
-	-- Git colors
-	git_add = "#62cc47",
-	git_change = "#4ff0ff",
-	git_delete = "#ed864a",
+	-- Git colors (gutter indicators)
+	git_add = "#009924",
+	git_change = "#1aabff",
+	git_delete = "#ffa14f",
 
 	-- Diagnostic colors
 	error = "#ff0d25",
 	warning = "#ffb300",
 	info = "#b39500",
 	hint = "#005380",
+
+	-- Comment colors
+	comment = "#7ec3e6",
+	doc_comment = "#499936",
+	doc_tag_value = "#a66e32",
+
+	-- UI colors
+	metadata = "#a9b837",
+	todo = "#adcc00",
+	match_brace = "#ffef28",
+	indent = "#666666",
+	indent_active = "#999999",
+	link = "#00aaff",
+	unused = "#b3b3b3",
+	injected_bg = "#153811",
+	color_column = "#808080",
+	custom_tag = "#00deb8",
 
 	-- Special
 	none = "NONE",
@@ -66,16 +83,18 @@ local function hi(group, opts)
 	if opts.bg then
 		cmd = cmd .. " guibg=" .. opts.bg
 	end
-	if opts.bold then
-		cmd = cmd .. " gui=bold"
+	if opts.sp then
+		cmd = cmd .. " guisp=" .. opts.sp
 	end
-	if opts.italic then
-		cmd = cmd .. " gui=italic"
+	local styles = {}
+	if opts.bold then table.insert(styles, "bold") end
+	if opts.italic then table.insert(styles, "italic") end
+	if opts.underline then table.insert(styles, "underline") end
+	if opts.undercurl then table.insert(styles, "undercurl") end
+	if opts.strikethrough then table.insert(styles, "strikethrough") end
+	if #styles > 0 then
+		cmd = cmd .. " gui=" .. table.concat(styles, ",")
 	end
-	if opts.underline then
-		cmd = cmd .. " gui=underline"
-	end
-
 	vim.cmd(cmd)
 end
 
@@ -88,9 +107,16 @@ hi("CursorLine", { bg = colors.bg_highlight })
 hi("Visual", { bg = colors.bg_visual })
 hi("Search", { bg = colors.bg_search })
 hi("IncSearch", { bg = colors.bg_search })
+hi("MatchParen", { fg = colors.match_brace, bold = true })
+hi("ColorColumn", { bg = colors.color_column })
+hi("Whitespace", { fg = colors.fg })
+hi("NonText", { fg = colors.fg })
+hi("SpecialKey", { fg = colors.fg_dark })
+hi("Todo", { fg = colors.todo, italic = true })
+hi("Underlined", { fg = colors.link, underline = true })
 
 -- Syntax highlighting
-hi("Comment", { fg = colors.cyan, italic = true })
+hi("Comment", { fg = colors.comment, italic = true })
 hi("Constant", { fg = colors.purple, italic = true })
 hi("String", { fg = colors.green })
 hi("Character", { fg = colors.green })
@@ -124,7 +150,7 @@ hi("Special", { fg = colors.orange })
 hi("SpecialChar", { fg = colors.orange })
 hi("Tag", { fg = colors.orange })
 hi("Delimiter", { fg = colors.orange, bold = true })
-hi("SpecialComment", { fg = colors.cyan })
+hi("SpecialComment", { fg = colors.comment })
 hi("Debug", { fg = colors.orange })
 
 -- Git signs
@@ -132,11 +158,18 @@ hi("GitSignsAdd", { fg = colors.git_add })
 hi("GitSignsChange", { fg = colors.git_change })
 hi("GitSignsDelete", { fg = colors.git_delete })
 
+-- Indent guides (indent-blankline.nvim)
+hi("IblIndent", { fg = colors.indent })
+hi("IblScope", { fg = colors.indent_active })
+hi("IndentBlanklineChar", { fg = colors.indent })
+hi("IndentBlanklineContextChar", { fg = colors.indent_active })
+
 -- Diagnostics
 hi("DiagnosticError", { fg = colors.error })
 hi("DiagnosticWarn", { fg = colors.warning })
 hi("DiagnosticInfo", { fg = colors.green1 })
 hi("DiagnosticHint", { fg = colors.hint })
+hi("DiagnosticUnnecessary", { fg = colors.unused, underline = true })
 
 -- LSP
 hi("LspReferenceText", { bg = "#3b0a" })
@@ -166,7 +199,11 @@ hi("@type", { fg = colors.teal, bold = true })
 hi("@namespace", { fg = colors.teal, bold = true })
 hi("@module", { fg = colors.teal, bold = true })
 hi("@type.builtin", { fg = colors.cyan, bold = true })
-hi("@comment", { fg = colors.grey, italic = true })
+hi("@comment", { fg = colors.comment, italic = true })
+hi("@comment.documentation", { fg = colors.doc_comment, italic = true })
+hi("@comment.todo", { fg = colors.todo, italic = true })
+hi("@attribute", { fg = colors.metadata })
+hi("@lsp.mod.deprecated", { strikethrough = true, sp = colors.error })
 hi("@punctuation.special", { fg = colors.blue, bold = true })
 hi("@punctuation.delimiter", { fg = colors.orange, bold = true })
 hi("@punctuation.bracket", { fg = colors.blue, bold = true })
@@ -192,12 +229,12 @@ hi("yamlConstant", { fg = colors.purple, italic = true })
 hi("yamlKeyValueDelimiter", { fg = colors.orange, bold = true })
 hi("yamlBlockCollectionItemStart", { fg = colors.orange, bold = true }) -- -
 hi("yamlBlockMappingDelimiter", { fg = colors.orange, bold = true }) -- :
-hi("yamlAnchor", { fg = colors.red1, bold = true })
-hi("yamlAlias", { fg = colors.red1, bold = true })
+hi("yamlAnchor", { fg = colors.yellow })
+hi("yamlAlias", { fg = colors.yellow })
 hi("yamlNodeTag", { fg = colors.teal })
 hi("yamlDocumentStart", { fg = colors.orange }) -- ---
 hi("yamlDocumentEnd", { fg = colors.orange }) -- ...
-hi("yamlComment", { fg = colors.grey, italic = true })
+hi("yamlComment", { fg = colors.comment, italic = true })
 hi("yamlEscape", { fg = colors.orange })
 
 -- Telescope
@@ -229,3 +266,32 @@ hi("DiffAdd", { bg = "#400f" })
 hi("DiffChange", { bg = "#99" })
 hi("DiffDelete", { bg = "#4d2e08" })
 hi("DiffText", { bg = "#661300" })
+
+-- HTML
+hi("htmlTag", { fg = colors.orange })
+hi("htmlEndTag", { fg = colors.orange })
+hi("htmlTagName", { fg = colors.orange })
+hi("htmlSpecialTagName", { fg = colors.orange })
+hi("htmlArg", { fg = colors.fg })
+hi("htmlString", { fg = colors.green })
+hi("htmlSpecialChar", { fg = colors.link, bold = true })
+hi("htmlH1", { fg = colors.fg, bold = true })
+hi("htmlH2", { fg = colors.fg, bold = true })
+hi("htmlH3", { fg = colors.fg, bold = true })
+hi("@tag", { fg = colors.orange })
+hi("@tag.builtin", { fg = colors.orange })
+hi("@tag.delimiter", { fg = colors.orange })
+hi("@tag.attribute", { fg = colors.fg })
+hi("@tag.html", { fg = colors.orange })
+hi("@tag.custom", { fg = colors.custom_tag })
+
+-- XML
+hi("xmlTag", { fg = colors.orange })
+hi("xmlEndTag", { fg = colors.orange })
+hi("xmlTagName", { fg = colors.orange })
+hi("xmlAttrib", { fg = colors.fg })
+hi("xmlString", { fg = colors.green })
+hi("xmlEntity", { fg = colors.link, bold = true })
+
+-- Injected language
+hi("InjectedLanguage", { bg = colors.injected_bg })
